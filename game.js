5bbb22767c2435985ff0c2e0fb5760aa8a552297
Thomas Ebert <thomas.ebert@te-online.net>
2015-07-02 16:05:32 +0200

    Use correct HTML5 functions.

--- game.js
+++ game.js
@@ -22,6 +22,13 @@ $(document).ready( function() {
     var currentDecision = 0;
 
     /**
+     * Disable Video controls
+     */
+    $('video').each(function(i, item) {
+        $(item)[0].controls = false;
+    });
+
+    /**
      * SOCKET CONNECTION
      */
     // Connect Web socket
@@ -30,34 +37,45 @@ $(document).ready( function() {
     socket.on('KINECT_stateChange', function (data) {
         // Wenn kinect changes
         //  wenn user == true und ext_teaser !playing
-        if(data.state && $('video[data-type=ext_teaser]:playing').size() == 0) {
+        if(data.state && $('video[data-type=ext_teaser]').vid_numPlaying() == 0) {
             //      fade teaser aus und ext_teaser ein
             $('video[data-type=teaser]').fadeOut(function() {
-                $(this).stop();
+                $(this).vid_stop();
             });
-            $('video[data-type=ext_teaser]').play().fadeIn();
-        } else if(!data.state && $('video[data-type=teaser]:playing').size() == 0) {
+            $('video[data-type=ext_teaser]').vid_play().fadeIn();
+        } else if(!data.state && $('video[data-type=teaser]').vid_numPlaying() == 0) {
              //  wenn user == false and teaser !playing
              //  fade teaser ein und ext_teaser aus
             $('video[data-type=ext_teaser]').fadeOut(function() {
-                $(this).stop();
+                $(this).vid_stop();
             });
-            $('video[data-type=teaser]').play().fadeIn();
+            $('video[data-type=teaser]').vid_play().fadeIn();
         }        
     });
 
     socket.on('MACHINE_hasCoin', function (data) {
         // Wenn money
-        //  wenn walking spielt
-        if($('video[data-type=walking]:playing').size() > 0) {
-            //  addiere Betrag zu value
+        // wenn walking spielt
+        if($('video[data-type=walking]').vid_numPlaying() > 0) {
+            // addiere Betrag zu value
             currentCredit += data.coin;
-            //      setze decision
-            currentDecision = availableCoins[data.coin];
+            // setze decision
+            currentDecision = availableCoins.(data.coin);
+            $('.currentCredit').text(parseMoney(currentCredit));
+        } else if($('video[data-type=teaser]').vid_numPlaying() > 0) {
+            $('video[data-type=teaser]').fadeOut(function() {
+                $(this).vid_stop();
+                $('video[data-type=walk][data-id='+currentLevel+']').fadeIn().vid_play();
+            });
+        } else if($('video[data-type=ext_teaser]').vid_numPlaying() > 0) {
+            $('video[data-type=ext_teaser]').fadeOut(function() {
+                $(this).vid_stop();
+                $('video[data-type=walk][data-id='+currentLevel+']').fadeIn().vid_play();
+            });
         } else {
-            //  sonst
-            //  addiere zu wrong money 
+            // sonst addiere zu wrong money 
             wrongCredit += data.coin;
+            $('.wrongCredit').text(parseMoney(wrongCredit));
         }             
     });
 
@@ -72,13 +90,13 @@ $(document).ready( function() {
         $(this).attr('data-callToAction', 'false');
         if(currentDecision > 0) {
             // Verstecke walking
-            $(this).stop().hide();
-            $('video[data-type=option][data-id='+currentDecision+']').show().play();
+            $(this).vid_stop().hide();
+            $('video[data-type=option][data-id='+currentDecision+']').show().vid_play();
         } else {
             // Blende walking aus
-            $(this).stop().fadeOut( function() {
+            $(this).vid_stop().fadeOut( function() {
                 // Zeige gameover mit id = 0 (Standard Gameover)
-                $('video[data-type=gameover][data-id=0]').fadeIn().play();
+                $('video[data-type=gameover][data-id=0]').fadeIn().vid_play();
             });
         }
     });
@@ -89,66 +107,106 @@ $(document).ready( function() {
         if(currentLevel + 1 <= numLevels) {
             currentLevel++;
             // Verstecke option
-            $(this).stop().hide();
+            $(this).vid_stop().hide();
             // Spiele nächstes walking
-            $('video[data-type=walking][data-id='+currentLevel+']').show().play();
+            $('video[data-type=walking][data-id='+currentLevel+']').show().vid_play();
         } else {
             // Verstecke option
-            $(this).stop().hide();
+            $(this).vid_stop().hide();
             // Zeige gameover mit id = 1 (Win Gameover)
-            $('video[data-type=option][data-id=1]').show().play();
-            /**
-             * Zeige hier ggf. noch einen Text mit dem currentCredit
-             * Gib hier das Geld zurück
-             */
+            $('video[data-type=option][data-id=1]').show().vid_play();
+            // Zeige hier ggf. noch einen Text mit dem currentCredit
+            $('.wintext').fadeIn();
+            // Gib hier das Geld zurück
             socket.emit('MACHINE__eject', true);
         }
     });
 
     // Wenn teaser on end
-    $(document).on('end', 'video[data-type=teaser]', function() {
+    $(document).on('ended', 'video[data-type=teaser]', function() {
         // rewind teaser and play again
+        $(this).vid_loop();
     });
 
     // Wenn ext_teaser on end
-    $(document).on('end', 'video[data-type=ext_teaser]', function() {
+    $(document).on('ended', 'video[data-type=ext_teaser]', function() {
         // rewind ext_teaser and play again
+        $(this).vid_loop();
     });
 
-    $(document).on('play', 'video[data-type=walking]', function() {
+    $(document).on('timeupdate', 'video[data-type=walking]', function() {
         // Wenn 400ms vor Ende
-        if((($(this).duration() - $(this).time()) <= decisionTime) && $(this).attr('data-callToAction') == "false") {
+        if((($(this).vid_duration() - $(this).vid_currentTime()) <= decisionTime) && $(this).attr('data-callToAction') == "false") {
             $('.optionIcons').fadeIn();
             $(this).attr('data-callToAction', 'true');
         }
     })
 
-    $(document).on('play', 'video[data-type=gameover]', function() {
+    $(document).on('timeupdate', 'video[data-type=gameover]', function() {
         // Wenn 400ms vor Ende
-        if((($(this).duration() - $(this).time()) <= 400) && $(this).attr('data-animated') == "false") {
+        if((($(this).vid_duration() - $(this).vid_currentTime()) <= 400) && $(this).attr('data-animated') == "false") {
             $(this).attr('data-animated', 'true');
             $(this).fadeOut( function() {
                 $(this).attr('data-animated', 'false');
-                $(this).stop();
+                $(this).vid_stop();
             });
-            $('video[data-type=teaser]').fadeIn().play();
+            $('.wintext').fadeOut();
+            $('video[data-type=teaser]').fadeIn().vid_play();
             currentLevel = 1;
             currentCredit = 0;
         }
     });
 
+    /**
+     * Extend jQuery for the HMTL5 video functions of DOM
+     * Iterate through each object and apply DOM-functions
+     */
     jQuery.fn.extend({
-      play: function() {
+      vid_play: function() {
         return this.each(function(i, item) {
           this.play();
         });
       },
-      stop: function() {
+      vid_stop: function() {
         return this.each(function() {
-          this.stop();
+          this.pause();
+          this.currentTime = 0;
         });
+      },
+      vid_pause: function() {
+        return this.each(function() {
+            this.pause();
+        });
+      },
+      vid_duration: function() {
+        // in seconds, but we want milliseconds
+        return this[0].duration * 1000;
+      },
+      vid_currentTime: function() {
+        // in seconds, but we want milliseconds
+        return this[0].currentTime * 1000;
+      },
+      vid_jumpTo: function(milliseconds) {
+        this[0].currentTime = second/1000;
+      },
+      vid_loop: function() {
+        return this.each(function(i, item) {
+          this.loop = true;
+        });
+      },
+      vid_playing: function() {
+        return !this[0].paused;
       }
     });
+
+    /**
+     * Parse cents in euro readable format
+     */
+    function parseMoney(cents) {
+        cents = ""+cents;
+        cents = cents.replace(".", ",");
+        return cents + " €";
+    }
     
 });
 
